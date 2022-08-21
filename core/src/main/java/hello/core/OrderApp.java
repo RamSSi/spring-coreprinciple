fbf6527e843d4dfb25bce3a0cc07837fe26a0037
RamSSi <001229aram@gmail.com>
2022-08-21 23:19:46 +0900

    객체 지향 원리 적용

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -1,30 +1,32 @@
 package hello.core;
 
-import hello.core.member.Member;
 import hello.core.member.Grade;
+import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
-public class MemberApp {
-
+public class OrderApp {
     public static void main(String[] args) {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        // AppConfig에서 설정한 컴포넌트들을 생성해 Spring Container에 넣어 관리하겠다는 의미
-        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService  = applicationContext.getBean("orderService", OrderService.class);
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member.getName());
-        System .out.println("findMember = " + findMember.getName());
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
 
-
+        System.out.println("order=" + order);
     }
 }
--- core/src/test/java/hello/core/order/CoreApplicationTests.java
+++ core/src/test/java/hello/core/order/CoreApplicationTests.java
@@ -1,4 +1,4 @@
-package hello.core;
+package hello.core.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
