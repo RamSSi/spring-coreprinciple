cd330736ec086836285294dd31416d2c8fc3426c
RamSSi <001229aram@gmail.com>
2022-08-30 17:41:51 +0900

    빈 생명주기 콜백 ing

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -1,2 +1,37 @@
-package hello.core.singleton;public class ConfigurationSingletonTest {
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderSerivce -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        // 구성정보로 등록이 되면 스프링 빈에 등록된다!
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean);
+    }
 }
--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -1,2 +1,18 @@
-package hello.core.singleton;public class SingletonService {
+package hello.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    private SingletonService() {
+
+    }   // 생성자가 private이므로 다른 곳에서 새로운 인스턴스 생성 불가
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
 }
